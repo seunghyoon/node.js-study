96db6a183e9e5a3e895c2408fa8a7397f77f8a32
hilov@naver.com <hilov@naver.com>
2023-05-04 11:53:35 +0900

    config add

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = require('./config/key')
+
+const { User } = require('./models/User')
+const bodyParser = require('body-parser');
+
+
+//content-type setting
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hilov:bdggCwdSkyw0ugiH@cluster0.w5i3xbm.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoUri,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected.....'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요.....MongoDB Connected.....')
+  res.send('Hello World! 안녕하세요.....MongoDB Connected...1..')
+})
+
+app.post('/register', (req, res) => {
+  console.log("req.body {}", req.body);
+  //회원가입 
+  const user = new User(req.body);
+  console.log(user);
+  user.save().then(()=>{
+    res.status(200).json({
+      success: true
+    })
+  }).catch((err)=>{
+    res.json({
+      success: false, err
+    })
+  })
+  /*
+  user.save((err, userInfo) =>{
+    if(err) return res.json({ 
+      success: false
+    })
+    return res.status(200).json({cl
+       success: true 
+    })
+    
+  });
+  */
 })
 
 app.listen(port, () => {
